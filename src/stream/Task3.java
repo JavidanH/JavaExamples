47efb3b05519ee350a551fcaa0b098b23bed1612
JavidanH <jahajiyev@gmail.com>
2026-03-25 17:09:38 +0400

    stream task2

---
+++ src/stream/Task3.java
@@ -0,0 +1,21 @@
+package stream;
+
+import java.util.List;
+
+public class Task3 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1,2,3,4,5);
+
+        int sum = numbers.stream().reduce(0,Integer ::sum);
+
+        int product = numbers.stream()
+                .reduce(1, (a, b) -> a * b);
+
+        String concat = numbers.stream().map(String :: valueOf)
+                .reduce("", (a, b) -> a + b);
+
+        System.out.println(sum);
+        System.out.println(product);
+        System.out.println(concat);
+    }
+}
